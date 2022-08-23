a2c53cff76b02be0784896103c1ad4ae78caf013
JP <cyborgninjaa@gmail.com>
2022-08-23 13:00:20 +1000

    making changes

--- flatpaks.sh
+++ flatpaks.sh
@@ -4,7 +4,7 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 sudo flatpak update
 
 sudo flatpak install -y flathub com.microsoft.Teams
-sudo flatpak install -y flathub com.github.eneshecan.WhatsAppForLinux
-sudo flatpak install -y flathub com.sindresorhus.Caprine
+# sudo flatpak install -y flathub com.github.eneshecan.WhatsAppForLinux
+# sudo flatpak install -y flathub com.sindresorhus.Caprine
 sudo flatpak install -y flathub org.onlyoffice.desktopeditors
 sudo flatpak install -y flathub com.discordapp.Discord
--- snaps.sh
+++ snaps.sh
@@ -3,3 +3,6 @@ sudo dnf install -y snapd
 
 # install keep presence https://github.com/carrot69/keep-presence
 sudo snap install keep-presence
+
+# install ferdi
+snap install ferdi
