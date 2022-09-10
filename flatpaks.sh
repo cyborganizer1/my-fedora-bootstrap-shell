9d07c380d503aa34cb2ad3d0a7d608e95dec86c1
JP <cyborgninjaa@gmail.com>
2022-09-10 11:55:59 +1000

    changes

--- flatpaks.sh
+++ flatpaks.sh
@@ -4,7 +4,8 @@ sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub
 sudo flatpak update
 
 sudo flatpak install -y flathub com.microsoft.Teams
-# sudo flatpak install -y flathub com.github.eneshecan.WhatsAppForLinux
+sudo flatpak install -y flathub com.github.eneshecan.WhatsAppForLinux # Issues - Paste clipboard images, laggy/freezes
 # sudo flatpak install -y flathub com.sindresorhus.Caprine
 sudo flatpak install -y flathub org.onlyoffice.desktopeditors
 sudo flatpak install -y flathub com.discordapp.Discord
+sudo flatpak install -y flathub me.hyliu.fluentreader
