2e0c259078eea49d3d81bc31d6819a8de11ce536
cyborganizer1 <68762902+cyborganizer1@users.noreply.github.com>
2022-12-05 19:30:37 +1100

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -18,10 +18,10 @@ sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.co
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install -y brave-browser
 
-# install vivaldi
-sudo dnf install -y dnf-utils
-sudo dnf config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo
-sudo dnf install -y vivaldi-stable
+# install vivaldi (Crashes when you pull tabs out, use Brave instead)
+# sudo dnf install -y dnf-utils
+# sudo dnf config-manager --add-repo https://repo.vivaldi.com/archive/vivaldi-fedora.repo
+# sudo dnf install -y vivaldi-stable
 
 # office 365 webapps -install edge 1st
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
