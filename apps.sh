a7413066223e7d69a3efb53238226915e0ea6569
cyborganizer1 <68762902+cyborganizer1@users.noreply.github.com>
2023-02-06 18:58:05 +1100

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -10,6 +10,13 @@
 # install jdownloader2
 # https://jdownloader.org/download/index
 
+# install thunderbird
+  sudo dnf install -y thunderbird
+
+# install birdtray (Birdtray is a system tray new mail notification for Thunderbird)
+  sudo dnf copr enable aerfanr/birdtray
+  sudo dnf install -y birdtray
+
 # install vscodium
 sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
 sudo printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
